9e074337ba667c6673668fef3cc4ebe0af0e706d
Cleboost <clement.balarot@gmail.com>
2025-01-30 20:14:55 +0100

    📇 refactor: remove file copying logic from tsup configuration

--- tsup-prod.config.ts
+++ tsup-prod.config.ts
@@ -1,5 +1,4 @@
 import { defineConfig } from "tsup";
-import fs from "fs";
 
 export default defineConfig({
   format: ["cjs"],
@@ -9,13 +8,4 @@ export default defineConfig({
   skipNodeModulesBundle: false,
   clean: true,
   minify: true,
-  onSuccess: async () => {
-    fs.copyFileSync("package.json", "dist/package.json");
-    fs.copyFileSync("README.md", "dist/README.md");
-    const packageString = fs.readFileSync("dist/package.json", "utf-8");
-    const packageJson = JSON.parse(packageString);
-    delete packageJson.devDependencies;
-    delete packageJson.scripts;
-    fs.writeFileSync("dist/package.json", JSON.stringify(packageJson, null, 2));
-  },
 });
